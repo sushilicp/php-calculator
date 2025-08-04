53fe173f5bd12f5c34535a9760d5fd5b81ca980f
Sushil <sushil.parajuli@veniosystems.com>
2025-08-04 17:32:42 +0545

    update coponser.lock and .json files

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'devops/php-calculator',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b84759e3bd788e19e8ba749b8cd7e8cc7e120440',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'devops/php-calculator' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b84759e3bd788e19e8ba749b8cd7e8cc7e120440',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
